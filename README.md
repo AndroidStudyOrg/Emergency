0e36b2db54ece845e6a32e00aa6426985f28c787
wbham <wbham97@naver.com>
2023-09-18 21:18:30 +0900

    [Feat] - 응급 의료정보 layout
    - FAB 누르면 다음 Activity 실행
    - 생년월일을 캘린더를 통해 입력 받는 layout
    - 혈액형 선택 시 RadioGroup과 RadioButton, 그리고 Spinner를 이용한 layout

--- README.md
+++ README.md
@@ -10,4 +10,5 @@
    - ConstraintLayout, ImageView, RadioButton, CheckBox, Spinner, DatePicker, Toolbar
  - CustomUI
  - Style-Theme
- - 일관성 있는 UI 활용
+ - 일관성 있는 UI 활용(style 만들기)
+ - 라디오 버튼들 중에 하나만 선택 가능하기에 라디오 그룹 안에 라디오 버튼들을 넣어줘야 한다.
--- app/src/main/java/com/example/emergency/MainActivity.kt
+++ app/src/main/java/com/example/emergency/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.emergency
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.emergency.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goInputActivity.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            intent.putExtra("intentMessage","응급의료정보")
+            startActivity(intent)
+        }
     }
 }
