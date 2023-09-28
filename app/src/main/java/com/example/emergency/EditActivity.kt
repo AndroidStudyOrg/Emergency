9c3d8e7aa1d9d6d12cd2b919f6efa3f625e5c9d7
wbham <wbham97@naver.com>
2023-09-29 01:05:18 +0900

    [Feat] - 데이터 저장(SharedPreference)
    - SharedPreference: 저장하려는 데이터 양이 적을 경우
    - Activity 생명주기를 고려한 데이터 및 UI 업데이트
    - 상수 전용 파일 생성

--- app/src/main/java/com/example/emergency/EditActivity.kt
+++ app/src/main/java/com/example/emergency/EditActivity.kt
@@ -2,10 +2,12 @@ package com.example.emergency
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.emergency.databinding.ActivityEditBinding
 
@@ -34,5 +36,45 @@ class EditActivity : AppCompatActivity() {
         }
 
         binding.etWarningValue.isVisible = binding.cbWarning.isChecked
+
+        binding.bntSave.setOnClickListener {
+            saveData()
+            // 저장 완료 후 Activity 종료
+            finish()
+        }
+    }
+
+    /**
+     * Context.MODE_PRIVATE: 파일을 생성한 앱에서만 접근 가능
+     */
+    private fun saveData() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+            putString(NAME, binding.etNameValue.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.etCallNumValue.text.toString())
+            putString(BIRTHDATE, binding.tvBirthhDayValue.text.toString())
+            putString(WARNING, getWarning())
+            apply()
+            /**
+             * commit을 해줄 수도, apply를 해줄 수도 있음
+             * commit: 얘를 가지고 있는 스레드를 막고, 저장이 완료된 후에 다음 동작을 함. 데이터를 저장하는 동안 사용자는 아무것도 못함
+             * apply: commit과는 다르게 비동기적으로 동작함. 저장할 때 해당 스레드가 아닌 다른 스레드를 열어서 작업함. 가능한한 apply로
+             */
+        }
+        Toast.makeText(this, "저장이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getBloodType(): String {
+        val bloodType = binding.spinnerBloodType.selectedItem.toString()
+        val bloodSign = if (binding.rbPlus.isChecked) "+" else "-"
+        return "$bloodSign$bloodType"
+    }
+
+    private fun getWarning(): String {
+        return if (binding.cbWarning.isChecked) {
+            binding.etWarningValue.text.toString()
+        } else {
+            ""
+        }
     }
 }
--- app/src/main/java/com/example/emergency/MainActivity.kt
+++ app/src/main/java/com/example/emergency/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.emergency
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.example.emergency.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,6 +17,27 @@ class MainActivity : AppCompatActivity() {
         binding.goInputActivity.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getDataUiUpdate()
+    }
+
+    private fun getDataUiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.tvNameValue.text = getString(NAME, "미정")
+            binding.tvBirthhDayValue.text = getString(BIRTHDATE, "미정")
+            binding.tvBloodTypeValue.text = getString(BLOOD_TYPE, "미정")
+            binding.tvCallNumValue.text = getString(EMERGENCY_CONTACT, "미정")
+            val warning = getString(WARNING, "미정")
+
+            binding.tvWarning.isVisible = warning.isNullOrEmpty().not()
+            binding.tvWarningValue.isVisible = warning.isNullOrEmpty().not()
+            if (!warning.isNullOrEmpty()) {
+                binding.tvWarningValue.text = warning
+            }
         }
     }
 }
